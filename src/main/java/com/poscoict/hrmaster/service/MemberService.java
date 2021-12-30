3a2170a3fb0dc8f754277421bcaef977828c1a10
goaldae <goaldae@naver.com>
2021-12-30 16:42:08 +0900

    Add HR service

--- src/main/java/com/poscoict/hrmaster/service/MemberService.java
+++ src/main/java/com/poscoict/hrmaster/service/MemberService.java
@@ -6,8 +6,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.poscoict.hrmaster.domain.member.Member;
-import com.poscoict.hrmaster.domain.member.MemberRepository;
+import com.poscoict.hrmaster.domain.employee.Employee;
+import com.poscoict.hrmaster.domain.employee.EmployeeRepository;
 import com.poscoict.hrmaster.web.dto.MemberDto;
 
 import lombok.AllArgsConstructor;
@@ -15,15 +15,15 @@ import lombok.AllArgsConstructor;
 @Service
 @AllArgsConstructor
 public class MemberService implements UserDetailsService {
-	private MemberRepository memberRepository;
+	private EmployeeRepository employeeRepository;
 
 	/** * 회원가입 처리 * @param memberDto * @return */
 	@Transactional
-	public Long joinUser(MemberDto memberDto) {
+	public Long joinUser(MemberDto MemberDto) {
 		BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
 		// 비밀번호 암호화 처리
-		memberDto.setPassword(passwordEncoder.encode(memberDto.getPassword()));
-		return memberRepository.save(memberDto.toEntity()).getId();
+		MemberDto.setPassword(passwordEncoder.encode(MemberDto.getPassword()));
+		return employeeRepository.save(MemberDto.toEntity()).getId();
 	}
 	
 	/**
@@ -31,7 +31,7 @@ public class MemberService implements UserDetailsService {
 	 * 갖는 UserDetails 인터페이스 반환 * @throws UsernameNotFoundException
 	 */
 	@Override
-	public Member loadUserByUsername(String email) throws UsernameNotFoundException {
-		return memberRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException((email)));
+	public Employee loadUserByUsername(String email) throws UsernameNotFoundException {
+		return employeeRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException((email)));
 	}
 }
