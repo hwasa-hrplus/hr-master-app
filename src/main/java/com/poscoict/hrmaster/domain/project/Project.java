df52fafcb061865fd285193fe0f840ce2fff2b5d
hansoohyun <1soohyun1@naver.com>
2022-01-05 09:10:39 +0900

    project entity 수정

--- src/main/java/com/poscoict/hrmaster/domain/project/Project.java
+++ src/main/java/com/poscoict/hrmaster/domain/project/Project.java
@@ -13,7 +13,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
-@Table(name = "workplace")
+@Table(name = "Project")
 public class Project {
 	@Id
 	@Column(name = "code", nullable = false)	
