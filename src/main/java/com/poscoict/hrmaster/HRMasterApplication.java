57af8e0dd082f2e6e0d7668ad7df033b505fb729
goaldae <goaldae@naver.com>
2022-01-08 10:55:57 +0900

    change springboot version to 2.3.1 for eureka and add eureka client

--- src/main/java/com/poscoict/hrmaster/HRMasterApplication.java
+++ src/main/java/com/poscoict/hrmaster/HRMasterApplication.java
@@ -1,8 +1,11 @@
 package com.poscoict.hrmaster;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class HRMasterApplication {
     public static void main( String[] args ) {
